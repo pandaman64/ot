4444e7fb3ffd514168279148e99624adae9f2840
pandaman <kointosudesuyo@infoseek.jp>
2018-03-17 20:54:35 +0900

    added actions for sending buffered operations/applying responses

--- src/client.rs
+++ src/client.rs
@@ -5,10 +5,11 @@ use util::*;
 extern crate futures;
 
 use self::futures::Future;
+use self::futures::FutureExt;
 
 pub trait Connection {
     type Error;
-    type Output: Future<Item = Operation, Error = Self::Error>;
+    type Output: Future<Item = (Id, Operation), Error = Self::Error>;
 
     fn get_latest_state(&self) -> (Id, Operation);
     fn send_operation(&self, operation: Operation) -> Self::Output;
@@ -58,22 +59,47 @@ impl<C: Connection> Client<C> {
         }
     }
 
-    pub fn send_to_server(&mut self) {
+    pub fn send_to_server(&mut self) -> Result<C::Output, ()> {
         use self::Client::*;
         if let Buffering { .. } = *self {
             if let Buffering { base_id, current_operation, connection } = std::mem::replace(self, Error("".into())) {
-                // TODO: do something
-                connection.send_operation(current_operation.clone());
+                let ret = connection.send_operation(current_operation.clone());
                 *self = WaitingForResponse {
                     base_id: base_id,
                     sent_operation: current_operation,
+                    // this might have to be Operation::new().retain(sent_operation.target_len())
+                    // because this is the operation that satisfies compose(sent, current) == sent 
                     current_operation: Operation::new(),
                     connection: connection,
-                }
+                };
+                Ok(ret)
             } else {
                 unreachable!();
             }
+        } else {
+            Err(())
         }
+    }
+
+    // this should be impl Future
+    pub fn apply_response<'a>(&'a mut self, response: C::Output) -> Box<Future<Item = (), Error = C::Error> + 'a> {
+        Box::new(response.map(move |(id, op)| {
+            use self::Client::*;
+            match std::mem::replace(self, Error("".into())) {
+                WaitingForResponse {
+                    sent_operation, current_operation, connection, ..
+                } => {
+                    let (current_, _) = transform(current_operation, op.clone());
+                    
+                    *self = Buffering {
+                        base_id: id,
+                        current_operation: compose(compose(sent_operation, op), current_),
+                        connection: connection,
+                    };
+                },
+                _ => unreachable!()
+            }
+        }))
     }
 }
 
