12924d246115becbc7e76ef76b598f7d761e7c5a
pandaman64 <kointosudesuyo@infoseek.jp>
2018-04-21 22:05:38 +0900

    implement linewise operations

---
+++ tests/charwise_util.rs
@@ -0,0 +1,54 @@
+extern crate ot;
+use ot::*;
+
+extern crate rand;
+
+pub fn random_string<R: rand::Rng>(rng: &mut R, len: usize) -> String {
+    rng.gen_iter::<char>().take(len).collect()
+}
+
+pub fn random_operation<R: rand::Rng>(rng: &mut R, original: &str) -> Operation {
+    use rand::distributions::{Range, Sample};
+
+    let mut op_type = Range::new(0, 3);
+
+    let mut ret = Operation::new();
+
+    let chars = original.chars().collect::<Vec<_>>();
+
+    let mut idx = 0;
+
+    while idx < chars.len() {
+        let mut op_len = Range::new(1, chars.len() - idx + 1);
+        match op_type.sample(rng) {
+            // Retain
+            0 => {
+                let len = op_len.sample(rng);
+                let mut bytes = 0;
+                for c in chars[idx..(idx + len)].iter() {
+                    bytes += c.len_utf8();
+                }
+                ret.retain(bytes);
+                idx += len;
+            }
+            // Insert
+            1 => {
+                let len = rng.gen_range(0, 10);
+                ret.insert(random_string(rng, len));
+            }
+            // Delete
+            2 => {
+                let len = op_len.sample(rng);
+                let mut bytes = 0;
+                for c in chars[idx..(idx + len)].iter() {
+                    bytes += c.len_utf8();
+                }
+                ret.delete(bytes);
+                idx += len;
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    ret
+}
--- tests/linewise.rs
+++ tests/linewise.rs
@@ -1,78 +1,87 @@
 extern crate ot;
-use ot::*;
+use ot::linewise::*;
 
 mod charwise_util;
-use charwise_util::*;
 
 extern crate rand;
 
 #[test]
 fn test_apply() {
-    let original = "こんにちは 世界";
+    let original = ["こんにちは", "世界"];
     let op = {
         let mut op = Operation::new();
-        op.retain("こんにちは".len())
+        op.retain(1)
             .insert("!".into())
-            .retain(" ".len())
-            .delete("世界".len())
-            .insert("社会".into());
+            .modify({
+                let mut op = ot::Operation::new();
+                op.delete("世界".len());
+                op.insert("社会".into());
+                op
+            });
         op
     };
 
-    assert_eq!(apply(original, &op), "こんにちは! 社会");
+    assert_eq!(apply(&original, &op), ["こんにちは", "!", "社会"]);
 }
 
 #[test]
 fn test_compose() {
-    let original = "こんにちは 世界";
+    let original = ["こんにちは",  "世界"];
     let first = {
         let mut op = Operation::new();
-        op.retain("こんにちは".len())
+        op.retain(1)
             .insert("!".into())
-            .retain(" ".len())
-            .delete("世界".len())
-            .insert("社会".into());
+            .modify({
+                let mut op = ot::Operation::new();
+                op.delete("世界".len());
+                op.insert("社会".into());
+                op
+            });
         op
     };
     let second = {
         let mut op = Operation::new();
-        op.delete("こんにちは".len())
+        op.delete(1)
             .insert("さようなら".into())
-            .retain("! 社会".len());
+            .retain(2);
         op
     };
 
+    use std::borrow::Borrow;
     assert_eq!(
-        apply(&apply(original, &first), &second),
-        apply(original, &compose(first.clone(), second.clone()))
+        apply(&apply(&original, &first).iter().map(Borrow::borrow).collect::<Vec<_>>(), &second),
+        apply(&original, &compose(first.clone(), second.clone()))
     );
     assert_eq!(
-        apply(&apply(original, &first), &second),
-        "さようなら! 社会"
+        apply(&apply(&original, &first).iter().map(Borrow::borrow).collect::<Vec<_>>(), &second),
+        ["さようなら", "!", "社会"]
     );
     assert_eq!(
-        apply(original, &compose(first, second)),
-        "さようなら! 社会"
+        apply(&original, &compose(first, second)),
+        ["さようなら", "!", "社会"]
     );
 }
 
 #[test]
 fn test_transform() {
-    let original = "こんにちは 世界";
+    let original = ["こんにちは", "世界"];
     let left = {
         let mut op = Operation::new();
-        op.retain("こんにちは".len())
+        op.retain(1)
             .insert("!".into())
-            .retain(" ".len())
-            .delete("世界".len())
-            .insert("社会".into());
+            .modify({
+                let mut op = ot::Operation::new();
+                op.delete("世界".len());
+                op.insert("社会".into());
+                op
+            });
         op
     };
     let right = {
         let mut op = Operation::new();
-        op.delete("こんにちは".len())
+        op.delete(1)
             .insert("さようなら".into())
-            .retain(" 世界".len());
+            .retain(1);
         op
     };
 
@@ -81,22 +90,74 @@ fn test_transform() {
     let composed_right = compose(right, left_);
 
     assert_eq!(
-        apply(original, &composed_left),
-        apply(original, &composed_right)
+        apply(&original, &composed_left),
+        apply(&original, &composed_right)
     );
-    assert_eq!(apply(original, &composed_left), "!さようなら 社会");
-    assert_eq!(apply(original, &composed_right), "!さようなら 社会");
+    assert_eq!(apply(&original, &composed_left), ["!", "さようなら", "社会"]);
+    assert_eq!(apply(&original, &composed_right), ["!", "さようなら", "社会"]);
+}
+
+fn random_lines<R: rand::Rng>(rng: &mut R, max_line_len: usize, line_num: usize) -> Vec<String> {
+    use rand::distributions::{Range, Sample};
+
+    let mut len = Range::new(0, max_line_len + 1);
+    (0..line_num).map(|_| {
+        let len = len.sample(rng);
+        charwise_util::random_string(rng, len)
+    }).collect()
+}
+
+fn random_operation<R: rand::Rng>(rng: &mut R, original: &[&str]) -> Operation {
+    use rand::distributions::{Range, Sample};
+
+    let mut op_type = Range::new(0, 4);
+    let mut ret = Operation::new();
+
+    let mut idx = 0;
+
+    while idx < original.len() {
+        let mut op_len = Range::new(1, original.len() - idx + 1);
+        match op_type.sample(rng) {
+            // Retain
+            0 => {
+                let len = op_len.sample(rng);
+                ret.retain(len);
+                idx += len;
+            }
+            // Insert
+            1 => {
+                let len = rng.gen_range(0, 10);
+                ret.insert(charwise_util::random_string(rng, len));
+            }
+            // Modify
+            2 => {
+                ret.modify(charwise_util::random_operation(rng, original[idx]));
+                idx += 1;
+            }
+            // Delete
+            3 => {
+                let len = op_len.sample(rng);
+                ret.delete(len);
+                idx += len;
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    ret
 }
 
 #[test]
 fn test_random_operation() {
     use rand::Rng;
+    use std::borrow::Borrow;
 
     let mut rng = rand::thread_rng();
     let original_len = rng.gen_range(32, 100);
-    let original = random_string(&mut rng, original_len);
+    let max_line_len = 30;
+    let original = random_lines(&mut rng, max_line_len, original_len);
 
-    let operation = random_operation(&mut rng, &original);
+    let operation = random_operation(&mut rng, &original.iter().map(Borrow::borrow).collect::<Vec<_>>());
 
     assert_eq!(operation.source_len(), original.len());
 }
@@ -104,19 +165,24 @@ fn test_random_operation() {
 #[test]
 fn fuzz_test_compose() {
     use rand::Rng;
+    use std::borrow::Borrow;
 
     let mut rng = rand::thread_rng();
 
     for _ in 0..100 {
         let original_len = rng.gen_range(32, 100);
-        let original = random_string(&mut rng, original_len);
+        let max_line_len = 30;
+        let original = random_lines(&mut rng, max_line_len, original_len);
+        let original = original.iter().map(Borrow::borrow).collect::<Vec<_>>();
 
         let first = random_operation(&mut rng, &original);
         let applied = apply(&original, &first);
+        let applied = applied.iter().map(Borrow::borrow).collect::<Vec<_>>();
 
         let second = random_operation(&mut rng, &applied);
 
         let double_applied = apply(&applied, &second);
+        println!("-------------------\nfirst = {:?}\nsecond = {:?}\ncomposed = {:?}", first, second, compose(first.clone(), second.clone()));
         let compose_applied = apply(&original, &compose(first, second));
 
         assert_eq!(double_applied, compose_applied);
@@ -126,12 +192,15 @@ fn fuzz_test_compose() {
 #[test]
 fn fuzz_test_transform() {
     use rand::Rng;
+    use std::borrow::Borrow;
 
     let mut rng = rand::thread_rng();
 
     for _ in 0..1000 {
         let original_len = rng.gen_range(32, 100);
-        let original = random_string(&mut rng, original_len);
+        let max_line_len = 30;
+        let original = random_lines(&mut rng, max_line_len, original_len);
+        let original = original.iter().map(Borrow::borrow).collect::<Vec<_>>();
 
         let left = random_operation(&mut rng, &original);
         let right = random_operation(&mut rng, &original);
@@ -144,7 +213,7 @@ fn fuzz_test_transform() {
         assert_eq!(apply(&original, &left), apply(&original, &right));
     }
 }
-
+/*
 #[macro_use]
 extern crate failure;
 extern crate futures;
@@ -268,4 +337,4 @@ fn test_client_server() {
     assert_eq!(client1.current_content().unwrap(), "!さようなら 世界");
     assert_eq!(client2.current_content().unwrap(), "!さようなら 世界");
 }
-
+*/
